1b3ee6599a39a81bad5f43a628d604a1c2bb455b
Jason Cai <jason.y.cai@gmail.com>
2021-10-22 09:15:19 -0400

    complete SoapBox smart contract

--- soapbox.sol
+++ soapbox.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.9;
+pragma solidity ^0.4.24;
 contract SoapBox {
 	// Our 'dict' of addresses that are approved to share opinions
 	mapping (address => bool) approvedSoapboxer;
@@ -8,7 +8,7 @@ contract SoapBox {
 	event OpinionBroadcast(address _soapboxer, string _opinion);
 
 	// Constructor function. Name matches the contract
-	function SoapBox() public {
+	constructor() public {
 	}
 
 	// Because this function is 'payable' it will be called when ether is sent
@@ -16,7 +16,7 @@ contract SoapBox {
 	function() public payable {
 		// msg is a special variable that contains information about the
 		// transaction.
-		if (msg.value > 20_000_000_000_000_000) {
+		if (msg.value > 20000000000000000) {
 			// if the value sent greater than 0.02 ether (in Wei)
 			// then add the sender's address to approvedSoapboxer
 			approvedSoapboxer[msg.sender] = true;
@@ -25,12 +25,25 @@ contract SoapBox {
 
 	// Our read-only function that checks whether the specified address is
 	// approved to post opinions. 
-	function isApproved(address _soapboxer) public view returns (bool approved) {
+	function isApproved(address _soapboxer) public view returns(bool approved) {
 		return approvedSoapboxer[_soapboxer];
 	}
 
-	// Read-only function that returns the current opinion
+	// Read-only function that returns the current opinion.
 	function getCurrentOpinion() public view returns(string) {
 		return opinion;
+	}
+
+	// Our function that modifies the state on the blockchain.
+	function broadcastOpinion(string _opinion) public returns (bool success) {
+		// Looking up the address of the sending will return false if the sender
+		// isn't approved
+		if (approvedSoapboxer[msg.sender]) {
+			opinion = _opinion;
+			emit OpinionBroadcast(msg.sender, opinion);
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
